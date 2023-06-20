99ec4257cf0f46be973ec332657b7721b48edd7c
Raphael-Soares <soaresphaell@gmail.com>
2023-06-20 08:48:42 -0300

    ascii e corzinha por que sim

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from colorama import Fore, Style
+
 def to_decimal(number, base):
     decimal = 0
     for num in number:
@@ -12,7 +14,22 @@ def to_base(decimal, base):
         decimal = decimal // base
     return result
 
+def print_ascii_interface():
+    ascii_interface = """
+    ____     _____     __  
+    |  _ \   / ____|   /_ | 
+    | |_) | | (___      | | 
+    |  _ <   \___ \     | | 
+    | |_) |  ____) |    | | 
+    |____/  |_____/     |_| 
+
+                                           
+    """
+    print(Fore.BLUE + ascii_interface + Style.RESET_ALL)
+
 def main():
+    print_ascii_interface()
+
     number = input("Digite um número: ")
     base = int(input("Digite a base atual: "))
     new_base = int(input("Digite a nova base: "))
@@ -20,10 +37,10 @@ def main():
     decimal = to_decimal(number, base)
 
     if new_base == 10:
-        print("Número convertido para base decimal:", decimal)
+        print(Fore.GREEN + "Número convertido para base decimal:", decimal + Style.RESET_ALL)
     else:
         converted_number = to_base(decimal, new_base)
-        print("Número convertido para base", new_base, ":", converted_number)
+        print(Fore.GREEN + f"Número convertido para base {new_base}:", converted_number + Style.RESET_ALL)
 
 if __name__ == "__main__":
     main()
