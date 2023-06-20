2f2cef1e184cca3028457852a5780cf4d8e08076
Raphael-Soares <soaresphaell@gmail.com>
2023-06-20 09:09:36 -0300

    cuidando dos erros

--- __main__.py
+++ __main__.py
@@ -2,8 +2,12 @@ from colorama import Fore, Style
 
 def to_decimal(number, base):
     decimal = 0
-    for num in number:
-        decimal = decimal * base + int(num)
+    try:
+        for num in number:
+            decimal = decimal * base + int(num)
+    except ValueError:
+        print("{red}Erro: O número informado não é válido para a base atual.{reset}".format(red=Fore.RED, reset=Style.RESET_ALL))
+        return None
     return decimal
 
 def to_base(decimal, base):
@@ -29,11 +33,18 @@ def print_ascii_interface():
 def main():
     print_ascii_interface()
 
-    number = input("{yellow}Digite um número:{reset} ".format(yellow=Fore.YELLOW, reset=Style.RESET_ALL))
-    base = int(input("{yellow}Digite a base atual:{reset} ".format(yellow=Fore.YELLOW, reset=Style.RESET_ALL)))
-    new_base = int(input("{yellow}Digite a nova base:{reset} ".format(yellow=Fore.YELLOW, reset=Style.RESET_ALL)))
+    try:
+        number = input("{yellow}Digite um número:{reset} ".format(yellow=Fore.YELLOW, reset=Style.RESET_ALL))
+        base = int(input("{yellow}Digite a base atual:{reset} ".format(yellow=Fore.YELLOW, reset=Style.RESET_ALL)))
+        new_base = int(input("{yellow}Digite a nova base:{reset} ".format(yellow=Fore.YELLOW, reset=Style.RESET_ALL)))
+    except ValueError:
+        print("{red}Erro: Entrada inválida. Certifique-se de inserir valores numéricos para as bases.{reset}".format(red=Fore.RED, reset=Style.RESET_ALL))
+        return
 
     decimal = to_decimal(number, base)
+
+    if decimal is None:
+        return
 
     if new_base == 10:
         print("{green}Número convertido para base decimal:{reset} {decimal}".format(green=Fore.GREEN, reset=Style.RESET_ALL, decimal=decimal))
